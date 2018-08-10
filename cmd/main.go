5c726b977c7cef213aa72d731533555b6f3ab547
Cheng Li <cli99@illinois.edu>
2018-08-10 11:43:54 -0400

    add root cmd

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	if err := cmd.FlopsInfoCmd.Execute(); err != nil {
+	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 
-	sourcepath "github.com/GeertJohan/go-sourcepath"
 	"github.com/Unknwon/com"
-	"github.com/pkg/errors"
-	"github.com/rai-project/dlperf/onnx"
 	"github.com/spf13/cobra"
 )
 
@@ -24,16 +23,14 @@ var (
 	raiSrcPath     string
 )
 
-var FlopsInfoCmd = &cobra.Command{
-	Use: "flops",
-	Aliases: []string{
-		"theoretical_flops",
-	},
-	Short: "Get flops information about the model",
-	PreRunE: func(cmd *cobra.Command, args []string) error {
+var rootCmd = &cobra.Command{
+	Use:   "dlperf",
+	Short: "dlperf",
+	Run: func(cmd *cobra.Command, args []string) {
 		if outputFormat == "automatic" {
 			outputFormat = ""
 		}
+
 		if outputFormat == "" && outputFileName != "" {
 			outputFormat = strings.TrimLeft(filepath.Ext(outputFileName), ".")
 			if outputFormat == "js" {
@@ -50,78 +47,25 @@ var FlopsInfoCmd = &cobra.Command{
 			}
 			outputFileName = filepath.Join(outputPath, dir)
 		}
-		return nil
-	},
-	RunE: func(c *cobra.Command, args []string) error {
-		run := func() error {
-
-			if modelPath == "" {
-				modelPath = filepath.Join(sourcepath.MustAbsoluteDir(), "..", "assets", "onnx_models", "mnist.onnx")
-			} else {
-				s, err := filepath.Abs(modelPath)
-				if err == nil {
-					modelPath = s
-				}
-			}
-
-			if !com.IsFile(modelPath) {
-				return errors.Errorf("file %v does not exist", modelPath)
-			}
-
-			net, err := onnx.NewOnnx(modelPath)
-			if err != nil {
-				return err
-			}
-
-			if fullFlops {
-				infos := net.LayerInformations()
-
-				writer := NewWriter(layer{}, humanFlops)
-				defer writer.Close()
-
-				for _, info := range infos {
-					writer.Row(
-						layer{
-							Name:             info.Name(),
-							Type:             info.OperatorType(),
-							FlopsInformation: info.Flops(),
-							Total:            info.Flops().Total(),
-						},
-					)
-				}
-
-				return nil
-			}
-
-			info := net.FlopsInformation()
-
-			writer := NewWriter(netSummary{}, humanFlops)
-			defer writer.Close()
-
-			writer.Row(netSummary{Name: "MultipleAdds", Value: info.MultiplyAdds})
-			writer.Row(netSummary{Name: "Additions", Value: info.Additions})
-			writer.Row(netSummary{Name: "Divisions", Value: info.Divisions})
-			writer.Row(netSummary{Name: "Exponentiations", Value: info.Exponentiations})
-			writer.Row(netSummary{Name: "Comparisons", Value: info.Comparisons})
-			writer.Row(netSummary{Name: "General", Value: info.General})
-			writer.Row(netSummary{Name: "Total", Value: info.Total()})
-
-			return nil
-		}
-
-		return run()
 	},
 }
 
 func init() {
-	FlopsInfoCmd.PersistentFlags().StringVar(&modelPath, "model_path", "", "path to the model prototxt file")
-	FlopsInfoCmd.PersistentFlags().BoolVar(&humanFlops, "human", false, "print flops in human form")
-	FlopsInfoCmd.PersistentFlags().BoolVar(&fullFlops, "full", false, "print all information about flops")
+	rootCmd.PersistentFlags().StringVar(&modelPath, "model_path", "", "path to the model prototxt file")
+	rootCmd.PersistentFlags().BoolVar(&humanFlops, "human", false, "print flops in human form")
+	rootCmd.PersistentFlags().BoolVar(&fullFlops, "full", false, "print all information about flops")
 
-	FlopsInfoCmd.PersistentFlags().BoolVar(&noHeader, "no_header", false, "show header labels for output")
-	FlopsInfoCmd.PersistentFlags().StringVarP(&outputFileName, "output", "o", "", "output file name")
-	FlopsInfoCmd.PersistentFlags().StringVarP(&outputFormat, "format", "f", "automatic", "print format to use")
+	rootCmd.PersistentFlags().BoolVar(&noHeader, "no_header", false, "show header labels for output")
+	rootCmd.PersistentFlags().StringVarP(&outputFileName, "output", "o", "", "output file name")
+	rootCmd.PersistentFlags().StringVarP(&outputFormat, "format", "f", "automatic", "print format to use")
 
 	goPath = com.GetGOPATHs()[0]
 	raiSrcPath = getSrcPath("github.com/rai-project")
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
