b3341da6218ff6c67839ecf2264ffedac63d137c
Abdul Dakkak <abduld@wolfram.com>
2018-08-15 10:31:52 -0400

    update

--- cmd/pattern.go
+++ cmd/pattern.go
@@ -1,16 +1,11 @@
 package cmd
 
 import (
-	"context"
 	"path/filepath"
 
 	sourcepath "github.com/GeertJohan/go-sourcepath"
-	"github.com/Unknwon/com"
-	zglob "github.com/mattn/go-zglob"
-	"github.com/pkg/errors"
 	"github.com/rai-project/dlperf/pkg/onnx"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -31,40 +26,9 @@ var patternCmd = &cobra.Command{
 			}
 		}
 
-		if !com.IsFile(modelPath) && !com.IsDir(modelPath) {
-			return errors.Errorf("file %v does not exist", modelPath)
-		}
-
-		var models []*onnx.Onnx
-		if com.IsFile(modelPath) {
-			model, err := onnx.New(modelPath)
-			if err != nil {
-				return err
-			}
-			models = []*onnx.Onnx{model}
-		}
-		if com.IsDir(modelPath) {
-			modelPaths, err := zglob.Glob(filepath.Join(modelPath, "**", "*.onnx"))
-			if err != nil {
-				return err
-			}
-			models = make([]*onnx.Onnx, len(modelPaths))
-			g, _ := errgroup.WithContext(context.Background())
-			for ii := range modelPaths {
-				idx := ii
-				g.Go(func() error {
-					path := modelPaths[idx]
-					model, err := onnx.New(path)
-					if err != nil {
-						return err
-					}
-					models[idx] = model
-					return nil
-				})
-			}
-			if err := g.Wait(); err != nil {
-				return err
-			}
+		models, err := readModels(modelPath)
+		if err != nil {
+			return err
 		}
 
 		subseqs, err := onnx.NodeSubsequences(patternLength, models...)
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,14 +1,60 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"path"
 	"path/filepath"
 	"strings"
 
 	"github.com/Unknwon/com"
+	zglob "github.com/mattn/go-zglob"
+	"github.com/pkg/errors"
+	"github.com/rai-project/dlperf/pkg/onnx"
 	"github.com/rai-project/utils"
+	"golang.org/x/sync/errgroup"
 )
+
+func readModels(modelPath string) ([]*onnx.Onnx, error) {
+
+	if !com.IsFile(modelPath) && !com.IsDir(modelPath) {
+		return nil, errors.Errorf("file %v does not exist", modelPath)
+	}
+
+	if com.IsFile(modelPath) {
+		model, err := onnx.New(modelPath)
+		if err != nil {
+			return nil, err
+		}
+		return []*onnx.Onnx{model}, nil
+	}
+
+	// is a directory
+
+	modelPaths, err := zglob.Glob(filepath.Join(modelPath, "**", "*.onnx"))
+	if err != nil {
+		return nil, err
+	}
+	models := make([]*onnx.Onnx, len(modelPaths))
+	g, _ := errgroup.WithContext(context.Background())
+	for ii := range modelPaths {
+		idx := ii
+		g.Go(func() error {
+			path := modelPaths[idx]
+			model, err := onnx.New(path)
+			if err != nil {
+				return err
+			}
+			models[idx] = model
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+	return models, nil
+
+}
 
 func getSrcPath(importPath string) (appPath string) {
 	paths := com.GetGOPATHs()
