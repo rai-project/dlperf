94bd8fab0c823aa6558c815fe879815aa436d9a6
Cheng Li <cli99@illinois.edu>
2018-08-16 11:41:38 -0400

    work

---
+++ pkg/layer/globalpooling.go
@@ -0,0 +1,65 @@
+package layer
+
+import (
+	"github.com/rai-project/dlperf/pkg"
+)
+
+type GlobalPooling struct {
+	Base `json:",inline,flatten,omitempty"`
+}
+
+func (GlobalPooling) Description() string {
+	return ``
+}
+
+func (c *GlobalPooling) InferShape(inputLayers []dlperf.Layer) {
+	c.SetInputShapes(getOutputShapes(inputLayers))
+
+	xShape := c.inputShapes[0]
+	xn := xShape[0]
+	xc := xShape[1]
+
+	yShape := dlperf.Shape{xn, xc, 1, 1}
+	c.SetOutputShapes([]dlperf.Shape{yShape})
+}
+
+func (c GlobalPooling) Information() dlperf.LayerInformation {
+	info := &Information{
+		Base: c.Base,
+		shape: dlperf.ShapeInformation{
+			InputShapes:  c.inputShapes,
+			OutputShapes: c.outputShapes,
+		},
+	}
+
+	if isAnyEmpty(c.outputShapes) {
+		log.WithField("layer", c.OperatorType()).Info("len(OutputShapes) is 0")
+		return info
+	}
+
+	checkNumber(c.InputShapes, []int{1}, c.OperatorType(), "number of inputs")
+	checkNumber(c.OutputShapes, []int{1}, c.OperatorType(), "number of outputs")
+
+	inputShapes := c.InputShapes()[0] // (N x C x ...)
+
+	nIn := inputShapes[0]
+	cIn := inputShapes[1]
+	hIn := inputShapes[2]
+	wIn := inputShapes[3]
+
+	flops := dlperf.FlopsInformation{}
+	switch c.operatorType {
+	case "globalmaxpool":
+		flops.Comparisons = wIn * hIn * cIn * nIn
+	case "globalaveragepool":
+		flops.Additions = wIn * hIn * cIn * nIn
+	}
+
+	info.flops = flops
+
+	return info
+}
+
+func init() {
+	dlperf.Register(&GlobalPooling{})
+}
