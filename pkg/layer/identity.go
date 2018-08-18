9b81411a419a30fbaf605ff8da48f0b4f4f4e1f5
Abdul Dakkak <abduld@wolfram.com>
2018-08-17 21:51:44 -0400

    fixes

---
+++ pkg/layer/identity.go
@@ -0,0 +1,43 @@
+package layer
+
+import (
+	"github.com/rai-project/dlperf/pkg"
+)
+
+// https://github.com/onnx/onnx/blob/master/docs/Operators.md#Identity
+
+type Identity struct {
+	*Base `json:",inline,flatten,omitempty"`
+}
+
+func (Identity) Description() string {
+	return ``
+}
+
+func (c *Identity) InferShape(inputLayers []dlperf.Layer) {
+	inputShapes := getOutputShapes(inputLayers)
+	c.SetOutputShapes(inputShapes)
+}
+
+func (c Identity) Information() dlperf.LayerInformation {
+	info := &Information{
+		Base: c.Base,
+		shape: dlperf.ShapeInformation{
+			InputShapes:  c.inputShapes,
+			OutputShapes: c.outputShapes,
+		},
+	}
+
+	if isAnyEmpty(c.outputShapes) {
+		log.WithField("layer", c.OperatorType()).Info("len(OutputShapes) is 0")
+		return info
+	}
+
+	info.flops = dlperf.FlopsInformation{}
+
+	return info
+}
+
+func init() {
+	dlperf.Register(&Identity{})
+}
