f5d35ec9bdd20bfd7ee905ee1bf46d1cd55894e4
Abdul Dakkak <abduld@wolfram.com>
2018-08-17 22:39:46 -0400

    updates

--- cmd/utils.go
+++ cmd/utils.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"time"
 
@@ -49,27 +48,8 @@ func dotToImage(dot []byte) (string, error) {
 }
 
 func newProgress(prefix string, count int) *pb.ProgressBar {
-	// get the new original progress bar.
-	//bar := pb.New(count).Prefix(prefix)
-	// TODO: set prefix of bar
-	bar := pb.New(count)
-	//bar.Set("prefix", prefix)
-
-	// Refresh rate for progress bar is set to 100 milliseconds.
+	bar := pb.New(count).Prefix(prefix)
 	bar.SetRefreshRate(time.Millisecond * 100)
-
-	// Use different unicodes for Linux, OS X and Windows.
-	switch runtime.GOOS {
-	case "linux":
-		// Need to add '\x00' as delimiter for unicode characters.
-		bar.Format("┃\x00▓\x00█\x00░\x00┃")
-	case "darwin":
-		// Need to add '\x00' as delimiter for unicode characters.
-		bar.Format(" \x00▓\x00 \x00░\x00 ")
-	default:
-		// Default to non unicode characters.
-		bar.Format("[=> ]")
-	}
 	bar.Start()
 	return bar
 }
