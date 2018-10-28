24032d8543684f72ae8d0e7a24fd46aa86ee6792
Abdul Dakkak <abduld@wolfram.com>
2018-10-28 16:52:00 -0500

    updates

--- pkg/layer/generate.go
+++ pkg/layer/generate.go
@@ -1,10 +1,8 @@
 //go:generate go get -v github.com/mailru/easyjson/...
-//go:generate easyjson -disallow_unknown_fields -snake_case -pkg .
 //go:generate go get github.com/valyala/quicktemplate/qtc
 //go:generate go get github.com/gotpl/gtfmt
 //go:generate go get -v github.com/mjibson/esc
 //go:generate go get github.com/wlbr/templify
-//go:generate esc -o generated_templates.go -pkg layer -prefix codegen -private codegen/scope
-//go:generate esc -o generated_tensorflow.go -pkg layer -prefix codegen -private codegen/tensorflow
-
+//go:generate esc -o generated_data.go -pkg layer -prefix codegen -private codegen
+//go:generate easyjson -disallow_unknown_fields -snake_case -pkg .
 package layer
