7fd9ac8ad37bd655d62450b872a6de612ced330b
Abdul Dakkak <abduld@wolfram.com>
2019-05-21 06:34:05 -0700

    update

--- cmd/downloadmodels.go
+++ cmd/downloadmodels.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"context"
-	"io"
-	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -11,73 +9,81 @@ import (
 	"strings"
 
 	"github.com/Unknwon/com"
-	"github.com/rai-project/archive"
+	getter "github.com/hashicorp/go-getter"
+	"github.com/rai-project/config"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 )
 
 func opsetName(url string) string {
-	re := regexp.MustCompile(`(?m).*opset_(\d+)\/.*`)
-	match := re.FindAllString(url, -1)
-	if len(match) == 0 {
+	re := regexp.MustCompile(`.*opset_(\d+)\/.*`)
+	match := re.FindStringSubmatch(url)
+	if len(match) <= 1 {
 		return ""
 	}
-	return "opset_" + match[0]
+	return "opset_" + match[1]
 }
 
 // downloadModelsCmd represents the downloadmodels command
 var downloadModelsCmd = &cobra.Command{
 	Use:     "downloadmodels",
 	Aliases: []string{"download"},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		g, _ := errgroup.WithContext(context.Background())
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if outputFileName == "automatic" || outputFileName == "" {
+			outputFileName = filepath.Join(config.App.TempDir, "dlperf")
+		}
+
 		if !com.IsDir(outputFileName) {
 			os.MkdirAll(outputFileName, os.ModePerm)
 		}
 
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		concurrencyLimit := 4
+		g, _ := errgroup.WithContext(context.Background())
+
 		progress := newProgress("> Downloading models", len(modelURLs))
 		defer progress.Finish()
 
+		sem := make(chan bool, concurrencyLimit)
 		for ii := range modelURLs {
 			url := modelURLs[ii]
+
+			sem <- true
+
 			g.Go(func() error {
 				defer progress.Increment()
+				defer func() { <-sem }()
+
 				outputDir := outputFileName
 				if opsetName(url) != "" {
 					outputDir = filepath.Join(outputFileName, opsetName(url))
 				}
-				resp, err := http.Get(url)
-				if err != nil {
-					log.WithError(err).WithField("url", url).Error("failed to download model")
-					return nil
-				}
-				defer resp.Body.Close()
+
+				targetFilePath := path.Base(url)
 				if strings.HasSuffix(url, ".onnx") {
-					filename := filepath.Join(outputDir, path.Base(url))
-					f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-					if err != nil {
-						return err
-					}
-					defer f.Close()
-					_, err = io.Copy(f, resp.Body)
-					if err != nil {
-						defer os.RemoveAll(filename)
-						log.WithError(err).WithField("url", url).Error("failed to write model")
-						return nil
-					}
-					return nil
-				}
-				err = archive.Unzip(resp.Body, outputDir, archive.GZipFormat())
-				if err != nil {
-					defer os.RemoveAll(outputFileName)
-					log.WithError(err).WithField("url", url).Error("failed to decompress model")
-					return nil
+					targetFilePath = path.Base(url)
 				}
 
+				targetFilePath = filepath.Join(outputDir, targetFilePath)
+
+				client := &getter.Client{
+					Src:  url,
+					Dst:  targetFilePath,
+					Pwd:  outputDir,
+					Mode: getter.ClientModeFile,
+				}
+				if err := client.Get(); err != nil {
+					return err
+				}
 				return nil
 			})
 		}
 
+		for i := 0; i < cap(sem); i++ {
+			sem <- true
+		}
 		if err := g.Wait(); err != nil {
 			return err
 		}
