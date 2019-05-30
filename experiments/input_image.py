e0928b946e41780f6741e656e25988656e6b2257
Abdul Dakkak <abduld@wolfram.com>
2019-05-30 14:57:23 +0100

    updates

--- experiments/input_image.py
+++ experiments/input_image.py
@@ -12,6 +12,31 @@ def preprocess_imagenet(img_data):
     return norm_img_data
 
 
+# this function is from yolo3.utils.letterbox_image
+def letterbox_image(image, size):
+    """resize image with unchanged aspect ratio using padding"""
+    iw, ih = image.size
+    w, h = size
+    scale = min(w / iw, h / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
+
+    image = image.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new("RGB", size, (128, 128, 128))
+    new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))
+    return new_image
+
+
+def yolo_preprocess(img):
+    model_image_size = (416, 416)
+    boxed_image = letterbox_image(img, tuple(reversed(model_image_size)))
+    image_data = np.array(boxed_image, dtype="float32")
+    image_data /= 255.0
+    image_data = np.transpose(image_data, [2, 0, 1])
+    image_data = np.expand_dims(image_data, 0)
+    return image_data
+
+
 # def get(model):
 #     np.random.seed(0)
 #     return np.asarray(np.random.uniform(model.shape), dtype=np.float32)
