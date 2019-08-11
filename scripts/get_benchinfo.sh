2b09d2ac78964eed86749054c60d152ea9465c00
Abdul Dakkak <abduld@wolfram.com>
2019-08-11 02:24:34 -0500

    update info

---
+++ scripts/get_benchinfo.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -x
+
+go build
+
+
+declare -a batch_sizes=(
+  1 \
+  2 \
+  4 \
+  8 \
+  16
+)
+
+declare -a machines=(
+  Tesla_K80 \
+  Tesla_V100-SXM2-16GB \
+  Quadro_RTX_6000 \
+  Tesla_P100-PCIE-16GB \
+  Tesla_P4
+)
+
+for batch_size in "${batch_sizes[@]}"
+do
+    echo $batch_size
+    for machine in "${machines[@]}"
+    do
+        echo $machine
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/benchinfo/batchsize_${batch_size}/${machine} --total=true --format=json --trim_layer_name=false
+    done
+done
