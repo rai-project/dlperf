ffffe2716c1c00f68760ae878bfaf176a498d024
Abdul Dakkak <abduld@wolfram.com>
2019-09-09 00:38:13 -0500

    generate fused activation

--- cmd/benchgen.go
+++ cmd/benchgen.go
@@ -78,8 +78,8 @@ var benchgenCmd = &cobra.Command{
 						b = l.FwdBenchmarkGenerator(dlperf.FwdBenchmarkArgsOption.ConvFwdType(dlperf.ConvFwdTypeConv))
 						b += "\n"
 						b += l.FwdBenchmarkGenerator(dlperf.FwdBenchmarkArgsOption.ConvFwdType(dlperf.ConvFwdTypeBias))
-						// b += "\n"
-						// b += l.FwdBenchmarkGenerator(dlperf.FwdBenchmarkArgsOption.ConvFwdType(dlperf.ConvFwdTypeConvFusedActivation))
+						b += "\n"
+						b += l.FwdBenchmarkGenerator(dlperf.FwdBenchmarkArgsOption.ConvFwdType(dlperf.ConvFwdTypeConvFusedActivation))
 					case "relu":
 						l := lyr.(*perflayer.Relu)
 						b = l.FwdBenchmarkGenerator()
