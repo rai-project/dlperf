6f43439a93cfe06e3f3628ef37ecc118585d59c4
Abdul Dakkak <abduld@wolfram.com>
2019-09-30 23:49:51 -0500

    reorder

--- scripts/get_benchinfo.sh
+++ scripts/get_benchinfo.sh
@@ -37,7 +37,7 @@ do
     for machine in "${machines[@]}"
     do
         echo $machine
-        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/benchinfo/batchsize_${batch_size}/${machine} --total=true --format=json --trim_layer_name=false
         ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/cudnn_advised_benchinfo/batchsize_${batch_size}/${machine} --total=true --format=json --trim_layer_name=false --choose_cudnn_heuristics
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/benchinfo/batchsize_${batch_size}/${machine} --total=true --format=json --trim_layer_name=false
     done
 done
