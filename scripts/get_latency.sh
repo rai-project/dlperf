6d9a2237dd8916d656e608f59f825612d808e08b
Abdul Dakkak <abduld@wolfram.com>
2019-10-05 03:36:00 -0500

    update

--- scripts/get_latency.sh
+++ scripts/get_latency.sh
@@ -53,9 +53,9 @@ declare -a machines=(
 
 
 declare -a machines=(
-  Quadro_RTX_6000 \
-  TITAN_V \
-  Tesla_T4 \
+  # Quadro_RTX_6000 \
+  # TITAN_V \
+  # Tesla_T4 \
   Tesla_V100-SXM2-16GB \
 )
 
@@ -66,8 +66,8 @@ do
     do
         echo $machine
       #  ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16
-      #  ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/serial_latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16
-       ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/fused_latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16 --fused
+       ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/serial_latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16
+      #  ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/fused_latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16 --fused
       #  ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/low_prec/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/serial_fused_latency_float16/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --datatype=float16 --fused
     done
 done
