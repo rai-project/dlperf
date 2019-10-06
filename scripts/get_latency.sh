2cbb9628e39db9313384f9b1e3f394b643151872
Abdul Dakkak <abduld@wolfram.com>
2019-10-06 12:49:37 -0500

    update

--- scripts/get_latency.sh
+++ scripts/get_latency.sh
@@ -23,40 +23,40 @@ declare -a batch_sizes=(
 declare -a machines=(
   # Tesla_K80 \
   # Tesla_M60 \
-  Quadro_RTX_6000 \
+  # Quadro_RTX_6000 \
   # TITAN_V \
   # TITAN_Xp \
-  # Tesla_T4 \
+  Tesla_T4 \
   # Tesla_V100-SXM2-16GB \
   # GRID_K520 \
 )
 
 
 
-# for batch_size in "${batch_sizes[@]}"
-# do
-#     echo $batch_size
-#     for machine in "${machines[@]}"
-#     do
-#         echo $machine
-#         ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/resnet50_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
-#         ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/resnet50_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
-#         ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/fused_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --fused
-#         ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/fused_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --fused
-#         # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/cudnn_advised_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --choose_cudnn_heuristics
-#         # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
-#         # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/cudnn_advised_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --choose_cudnn_heuristics
-#         # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
-#     done
-# done
+for batch_size in "${batch_sizes[@]}"
+do
+    echo $batch_size
+    for machine in "${machines[@]}"
+    do
+        echo $machine
+        # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/resnet50_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
+        # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/resnet50_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
+        # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/fused_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --fused
+        # ./dlperf benchinfo --model_path ~/data/carml/dlperf/ResNet50-v1/ --benchmark_database results/resnet50/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/fused_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --fused
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/cudnn_advised_serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --choose_cudnn_heuristics
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=serial --output_file=./assets/serial_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/cudnn_advised_latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false --choose_cudnn_heuristics
+        ./dlperf benchinfo --model_path ~/data/carml/dlperf/ --benchmark_database results/${machine}/${batch_size}.json --short=false --batch_size=${batch_size} --human=false --strategy=parallel --output_file=./assets/latency/batchsize_${batch_size}/${machine} --total=true --format=csv --trim_layer_name=false
+    done
+done
 
 
 
 declare -a machines=(
-  Quadro_RTX_6000 \
-  TITAN_V \
+  # Quadro_RTX_6000 \
+  # TITAN_V \
   Tesla_T4 \
-  Tesla_V100-SXM2-16GB \
+  # Tesla_V100-SXM2-16GB \
 )
 
 for batch_size in "${batch_sizes[@]}"
